ace34955abf1af913ba5c0c88f384ef94dde43c0
Araz <71676938+arazn845@users.noreply.github.com>
2022-09-18 21:40:05 +1000

    Add files via upload

---
+++ Lshape.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 532c3d40-3746-11ed-313b-5da6de95256e
+md"""
+$$e =$$
+"""
+
+# ╔═╡ d361e47d-5a20-4608-bf43-14d867aa5ee8
+md"""
+$$E =$$
+"""
+
+# ╔═╡ Cell order:
+# ╟─532c3d40-3746-11ed-313b-5da6de95256e
+# ╟─d361e47d-5a20-4608-bf43-14d867aa5ee8
