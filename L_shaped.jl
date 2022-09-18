25c00afc8177a41f7ee70f12c6a2f842855efcec
Araz <71676938+arazn845@users.noreply.github.com>
2022-09-18 21:50:25 +1000

    Add files via upload

---
+++ L_shaped.jl
@@ -0,0 +1,48 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 870d007a-f2a0-4313-a1eb-d7af267b4b8a
+md"""
+# Optimality cuts
+
+### general type 
+
+$$θ ≥ e_l - E_l x$$ 
+
+"""
+
+# ╔═╡ 532c3d40-3746-11ed-313b-5da6de95256e
+md"""
+$$e = \sum_{i=1}^{K} p_i λ_i h_i$$
+"""
+
+# ╔═╡ d361e47d-5a20-4608-bf43-14d867aa5ee8
+md"""
+$$E = \sum_{i=1}^{K} p_i λ_i h_i$$
+"""
+
+# ╔═╡ c99b15e6-2e2f-4579-9f08-853d0da77dae
+md"""
+# Example
+"""
+
+# ╔═╡ 1cb458c7-7d82-44d9-900d-d2f8bd676b10
+md"""
+### Stage 1
+
+$$\min 100 x_1 + 150 x_2$$
+$$x_1 + x_2 ≤ 120$$
+$$- x_1 ≤ -40$$
+$$- x_2 ≤ -20$$
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─870d007a-f2a0-4313-a1eb-d7af267b4b8a
+# ╟─532c3d40-3746-11ed-313b-5da6de95256e
+# ╟─d361e47d-5a20-4608-bf43-14d867aa5ee8
+# ╟─c99b15e6-2e2f-4579-9f08-853d0da77dae
+# ╟─1cb458c7-7d82-44d9-900d-d2f8bd676b10
